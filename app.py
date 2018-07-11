e17f601b05d9bc218936ef4144d8b26335d08ccd
Alex Bozarth <ajbozart@us.ibm.com>
2018-07-11 14:26:04 -0700

    Include Mini Web App in API Server
    This imports the [Mini Web App v2 Release](https://github.com/CODAIT/MAX-Object-Detector-Web-App/releases)
    of the [MAX Object Detector Web App](https://github.com/CODAIT/MAX-Object-Detector-Web-App)
    and includes it in the API server.
    
    The min web app is mounted at `/app` and documentation is added to the README.

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ import os
 from flask import Flask
 from api import api
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 # load default config
 app.config.from_object('config')
 # load override config if exists
 if 'APP_CONFIG' in os.environ:
-	app.config.from_envvar('APP_CONFIG')
+    app.config.from_envvar('APP_CONFIG')
 api.init_app(app)
+
+@app.route('/app/')
+def web_app():
+    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
         app.run(host='0.0.0.0')
