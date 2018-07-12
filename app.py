e3ec687acbe290fb68757bb4b12253590aa0bfa0
Alex Bozarth <ajbozart@us.ibm.com>
2018-07-11 17:51:17 -0700

    Added ability to disable mini web app
    By setting the DISABLE_WEB_APP env var (to anything) the web app will not be availble

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ import os
 from flask import Flask
 from api import api
 
-app = Flask(__name__, static_url_path='')
+if 'DISABLE_WEB_APP' not in os.environ:
+    app = Flask(__name__, static_url_path='')
+else:
+    app = Flask(__name__)
+
 # load default config
 app.config.from_object('config')
 # load override config if exists
@@ -10,9 +14,10 @@ if 'APP_CONFIG' in os.environ:
     app.config.from_envvar('APP_CONFIG')
 api.init_app(app)
 
-@app.route('/app/')
-def web_app():
-    return app.send_static_file('index.html')
+if 'DISABLE_WEB_APP' not in os.environ:
+    @app.route('/app/')
+    def web_app():
+        return app.send_static_file('index.html')
 
 if __name__ == '__main__':
         app.run(host='0.0.0.0')
