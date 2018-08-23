a353093539b682e0dda3b355a7ee9b9522c5ed24
Brendan Dwyer <brendan.dwyer@ibm.com>
2018-08-23 12:31:55 -0700

    switched to requests

--- tests/test.py
+++ tests/test.py
@@ -1,22 +1,18 @@
 import pytest
-import pycurl
-import io
-import json
+import requests
 
 
 def test_response():
-    c = pycurl.Curl()
-    b = io.BytesIO()
-    c.setopt(pycurl.URL, 'http://localhost:5000/model/predict')
-    c.setopt(pycurl.HTTPHEADER, ['Accept:application/json', 'Content-Type: multipart/form-data'])
-    c.setopt(pycurl.HTTPPOST, [('image', (pycurl.FORM_FILE, "assets/baby-bear.jpg"))])
-    c.setopt(pycurl.WRITEFUNCTION, b.write)
-    c.perform()
-    assert c.getinfo(pycurl.RESPONSE_CODE) == 200
-    c.close()
 
-    response = b.getvalue()
-    response = json.loads(response)
+    model_endpoint = 'http://localhost:5000/model/predict'
+    file_path = 'assets/baby-bear.jpg'
+
+    with open(file_path, 'rb') as file:
+        file_form = {'image': (file_path, file, 'image/jpeg')}
+        r = requests.post(url=model_endpoint, files=file_form)
+
+    assert r.status_code == 200
+    response = r.json()
 
     assert response['status'] == 'ok'
 
