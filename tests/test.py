c03002b39b32d5fd56ce5eb0200dc3fba3d18b12
Brendan Dwyer <brendan.dwyer@ibm.com>
2018-10-16 16:17:39 -0700

    Test Other Endpoints - test other endpoints - increase travis sleep time from 10 to 30 secs just to be safe - show download file progress - install from requirements.txt (resolves #23) - verify md5 hashes of downloaded files - update the swagger screenshot (resolves #27)

--- tests/test.py
+++ tests/test.py
@@ -2,8 +2,34 @@ import pytest
 import requests
 
 
-def test_response():
+def test_swagger():
 
+    model_endpoint = 'http://localhost:5000/swagger.json'
+
+    r = requests.get(url=model_endpoint)
+    assert r.status_code == 200
+    assert r.headers['Content-Type'] == 'application/json'
+
+    json = r.json()
+    assert 'swagger' in json
+    assert json.get('info') and json.get('info').get('title') == 'Model Asset Exchange Server'
+
+
+def test_metadata():
+
+    model_endpoint = 'http://localhost:5000/model/metadata'
+
+    r = requests.get(url=model_endpoint)
+    assert r.status_code == 200
+
+    metadata = r.json()
+    assert metadata['id'] == 'ssd_mobilenet_v1_coco_2017_11_17-tf-mobilenet'
+    assert metadata['name'] == 'ssd_mobilenet_v1_coco_2017_11_17 TensorFlow Model'
+    assert metadata['description'] == 'ssd_mobilenet_v1_coco_2017_11_17 TensorFlow model trained on MobileNet'
+    assert metadata['license'] == 'ApacheV2'
+
+
+def test_predict():
     model_endpoint = 'http://localhost:5000/model/predict'
     file_path = 'assets/baby-bear.jpg'
 
