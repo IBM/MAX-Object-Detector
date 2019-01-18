9b0299be9029106b856ffaf99780c90676e7b9b7
Simon Plovyt <simon@plovyt.com>
2019-01-18 11:15:45 -0800

    Compatible changes for the proposed new version of the framework

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from maxfw.core import MAXApp
 from api import ModelMetadataAPI, ModelPredictAPI
+from config import API_TITLE, API_DESC, API_VERSION
 
-max_app = MAXApp()
+max_app = MAXApp(API_TITLE, API_DESC, API_VERSION)
 max_app.add_api(ModelMetadataAPI, '/metadata')
 max_app.add_api(ModelPredictAPI, '/predict')
 max_app.run()
