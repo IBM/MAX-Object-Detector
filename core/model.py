cf02de9f063515206e1d1141f1128f272e9dac33
Brendan Dwyer <brendan.dwyer@ibm.com>
2019-07-16 16:41:14 -0700

    use tf session context

--- core/model.py
+++ core/model.py
@@ -24,22 +24,22 @@ class ModelWrapper(MAXModelWrapper):
         logger.info('Loading model from: {}...'.format(model_file))
         detection_graph = tf.Graph()
         graph = tf.Graph()
-        _ = tf.Session(graph=detection_graph)
-        # load the graph ===
-        # loading a (frozen) TensorFlow model into memory
+        with tf.Session(graph=detection_graph):
+            # load the graph ===
+            # loading a (frozen) TensorFlow model into memory
 
-        with graph.as_default():
-            od_graph_def = tf.GraphDef()
-            with tf.gfile.GFile(model_file, 'rb') as fid:
-                serialized_graph = fid.read()
-                od_graph_def.ParseFromString(serialized_graph)
-                tf.import_graph_def(od_graph_def, name='')
+            with graph.as_default():
+                od_graph_def = tf.GraphDef()
+                with tf.gfile.GFile(model_file, 'rb') as fid:
+                    serialized_graph = fid.read()
+                    od_graph_def.ParseFromString(serialized_graph)
+                    tf.import_graph_def(od_graph_def, name='')
 
-            # loading a label map
-            label_map = label_map_util.load_labelmap(label_file)
-            categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,
-                                                                        use_display_name=True)
-            category_index = label_map_util.create_category_index(categories)
+                # loading a label map
+                label_map = label_map_util.load_labelmap(label_file)
+                categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,
+                                                                            use_display_name=True)
+                category_index = label_map_util.create_category_index(categories)
 
         # set up instance variables
         self.graph = graph
