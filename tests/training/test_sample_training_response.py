2b0d07fc6219e2e7f0e7aa8bbe2e4be4ae4eb108
Saishruthi <saishruthi.tn@ibm.com>
2020-08-04 11:39:50 -0700

    Training Update (#145)
    * split requirement file
    
    * add packages
    
    * update sample image name
    
    * update check condition
    
    * update scripts as per new OD model
    
    * update metadata
    
    * flake update
    
    * flake update

--- tests/training/test_sample_training_response.py
+++ tests/training/test_sample_training_response.py
@@ -39,9 +39,9 @@ def test_metadata():
     assert r.status_code == 200
 
     metadata = r.json()
-    assert metadata['id'] == 'ssd_mobilenet_v1_coco_2017_11_17-tf-mobilenet'
-    assert metadata['name'] == 'ssd_mobilenet_v1_coco_2017_11_17 TensorFlow Model'
-    assert metadata['description'] == 'ssd_mobilenet_v1_coco_2017_11_17 TensorFlow model trained on MobileNet'
+    assert metadata['id'] == 'object-detector-ssd_mobilenet_v1'
+    assert metadata['name'] == 'ssd_mobilenet_v1 TensorFlow Object Detector Model'
+    assert metadata['description'] == 'ssd_mobilenet_v1 TensorFlow object detector model'
     assert metadata['type'] == 'Object Detection'
     assert metadata['source'] == 'https://developer.ibm.com/exchanges/models/all/max-object-detector/'
     assert metadata['license'] == 'ApacheV2'
@@ -49,7 +49,7 @@ def test_metadata():
 
 def test_predict():
     model_endpoint = 'http://localhost:5000/model/predict'
-    file_path = 'samples/data_1.jpg'
+    file_path = 'samples/a-pen-i-am.jpg'
 
     with open(file_path, 'rb') as file:
         file_form = {'image': (file_path, file, 'image/jpeg')}
@@ -59,11 +59,9 @@ def test_predict():
     response = r.json()
 
     assert response['status'] == 'ok'
-
-    #  Teddy Bear
-    # assert response['predictions'][0]['label_id'] == '88'
-    assert response['predictions'][0]['label'] == 'toy'
-    # assert response['predictions'][0]['probability'] > 0.95
+    # Training run uses fewer samples and epochs to train. Results have randomness due to this.
+    # Checking for all the new classes used in the sample data.
+    assert response['predictions'][0]['label'] in ('toy', 'pen')
 
 
 if __name__ == '__main__':
